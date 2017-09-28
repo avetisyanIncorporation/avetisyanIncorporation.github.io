837a32fe567a7f39da66160f50e2abab01779a5f
avetisyanIncorporation <pargev.avetisian@gmail.com>
2017-09-28 13:00:35 +0300

    q

--- msg1.js
+++ msg1.js
@@ -1,34 +1,10 @@
-function showMessage(){
-
-	 function show() {
-  alert("Hello, " + userYear + " years old human");
-}
-
-var userYear = prompt("How are you old?","1");
-if(userYear == 19){
-alert("My admin of your age");
-var isAdmin = confirm("Are you admin?");
-
-//isAdmin ? var pass = prompt("Enter password","") : pass == "qwe" ? alert("Hello, my lovely admin") : alert("Do not lie me. You didn't my admin. Fuck you!");
-
-if(isAdmin){
-var pass = prompt("Enter password","");
-
-if(pass == "qwe"){
-alert("Hello, my lovely admin");
-}
-else{
-alert("Do not lie me. You didn't my admin. Fuck you!");
-}	
-
-}
-else{
-show();
-}
-
-}else if(userYear == null){}
-else{
-show();
-}
-
-}
+//alert( parseFloat("12Hello :)"));
+      
+var p = document.getElementById("banner");
+            setInterval(function() {
+                if (p.style.fontSize != "2em") {
+                    p.style.fontSize = "2em";
+                } else {
+                    p.style.fontSize = "3em";
+                }
+            }, 2000);
