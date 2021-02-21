0fdfd36c9e6add1c7474b50b7e6ed7465b4585ce
Аветисян Паргев <pargev.avetisyan@lanit-tercom.com>
2021-02-21 13:00:33 +0300

    eyez

--- js/eyez.js
+++ js/eyez.js
@@ -2,15 +2,13 @@ $(document).ready(function() {
 
     "use strict";
 
-    $('body').on('click', '#lightTumbler', function () {
-        if ($(this).hasClass('dark-theme')) {
-            $('.light').removeClass('hidden');
-            $('.dark').addClass('hidden');
-            $(this).removeClass('dark-theme');
-        } else {
-            $('.light').addClass('hidden');
+    $('body').on('click', '.lightTumbler', function () {
+        if ($(this).hasClass('light')) {
+            $(this).addClass('hidden');
             $('.dark').removeClass('hidden');
-            $(this).addClass('dark-theme');
+        } else if ($(this).hasClass('dark')) {
+            $(this).addClass('hidden');
+            $('.light').removeClass('hidden');
         }
     });
 
