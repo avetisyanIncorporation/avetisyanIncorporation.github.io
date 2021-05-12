268aaaa5a8d1171e30aad56b6c7c4213a0e743c2
Аветисян Паргев <pargev.avetisyan@lanit-tercom.com>
2021-05-13 00:00:12 +0300

    eyez

--- js/eyez.js
+++ js/eyez.js
@@ -2,29 +2,48 @@ $(document).ready(function() {
 
     "use strict";
 
-    // function set_body_height() {
-    //     $('body').height($(window).height());
+    function set_body_height() {
+        $('.backgr').height($(window).height());
+        $('.backgr').width($(window).width());
+        $('.eye').height($(window).height() * 0.9);
+        $('.eye').height($(window).height() * 0.9);
+    }
+    $(window).bind('resize', set_body_height);
+    set_body_height();
+
+    // var eyes = $('div');
+    // var animate = function(element) {
+    //     element.find('.light').toggleClass('hidden');
+    //     element.find('.dark').toggleClass('hidden');
+    // };
+    //
+    // function random() {
+    //     var min = 0;
+    //     var max = eyes.length + 1;
+    //     let rand = min + Math.random() * (max - min + 1);
+    //     return Math.round(rand);
     // }
-    // $(window).bind('resize', set_body_height);
-    // set_body_height();
 
-    var eyes = $('td');
-    var animate = function(element) {
-        element.find('.light').toggleClass('hidden');
-        element.find('.dark').toggleClass('hidden');
-    };
 
-    function random() {
-        var min = 0;
-        var max = eyes.length + 1;
-        let rand = min + Math.random() * (max - min + 1);
-        return Math.round(rand);
-    }
 
-    var i = 0;
-    for (;i<1000;i++) {
-        setTimeout(animate, 500 * i, $(eyes[random()]));
-    }
+    // var i = 0;
+    // for (;i<1000;i++) {
+        // setTimeout(animate, 250 * i, $(eyes[random()]));
+    // }
+
+    setTimeout(function(){
+        $('#coin').addClass('animation1080');
+    }, 100);
+
+    $('#coin').on('click', function(){
+
+        $('#coin').removeClass();
+
+        setTimeout(function(){
+            $('#coin').addClass('animation1080');
+        }, 100);
+
+    });
 
     // $('body').on('click', '.lightTumbler', function () {
     //     if ($(this).hasClass('light')) {
